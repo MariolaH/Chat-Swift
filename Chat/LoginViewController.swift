fac45c9cfa4a5b669c5f17691d7ca7d60fe18139
Mariola <mariolahullings@gmail.com>
2024-01-14 16:17:53 -0500

    Added functionality to the login screen

--- Chat/LoginViewController.swift
+++ Chat/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -14,11 +15,24 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            //the user's account data will live in the authResult objest that we get back, it is used inside the closure block  or can check to see if there were any errors
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                    //if error is nil proced with next code block, the login
+                    AlertViewController.showAlert(self!, title: "Login Failed", message: e.localizedDescription)
+                } else {
+                    self?.performSegue(withIdentifier: Constants.loginSegue, sender: self)
+                }
+                
+            }
+        }
     }
     
 }
+
