7f628e38fbd8f719d11883726d13e8eb2c64c6d2
Mariola <mariolahullings@gmail.com>
2024-01-16 10:44:01 -0500

    Added messsage structure that is the model for creating messages

---
+++ Chat/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  Chat
+//
+//  Created by Mariola Hullings on 1/16/24.
+//
+//Messsage structure that is the model for creating messages
+
+import Foundation
+
+struct Message {
+    //contain email address of the person who sent the message
+    let sender: String
+    //contains the body of the message
+    let body: String
+}
