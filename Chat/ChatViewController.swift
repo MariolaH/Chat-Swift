488d2bd13dddaeac4cf9cb4b1f422bc812e337d4
Mariola <mariolahullings@gmail.com>
2024-01-20 22:13:24 -0500

    Retrieving data from Firestore

--- Chat/ChatViewController.swift
+++ Chat/ChatViewController.swift
@@ -17,11 +17,7 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var messageTextfield: UITextField!
     //Constains an array of message objects
     //messages has a data type of Message which is the data type that is used to structure the messages(Message struct) so they can have a body and sender
-    let messages: [Message] = [
-        Message(sender: "1@2.com", body: "Hi"),
-        Message(sender: "a@b.com", body: "Hello"),
-        Message(sender: "1@2.com", body: "What's up?")
-    ]
+    var messages: [Message] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,8 +26,44 @@ class ChatViewController: UIViewController {
         title = "⚡️ Chat"
         navigationItem.hidesBackButton = true
         tableView.register(UINib(nibName: Constants.cellNibName, bundle: nil), forCellReuseIdentifier: Constants.cellIdentifier)
+        loadMessages()
     }
-
+    
+    //this method pulls up all the current data inside the database.
+    //Then going to use it to populate the tableView
+    func loadMessages() {
+        messages = []
+        db.collection(Constants.FStore.collectionName).getDocuments { (querySnapshot, error) in
+            if let e = error {
+                print("There was an issue retrieving data from Firestore. \(e)")
+            } else {
+                //.documnet - array called documents because this contains the actual documents that saved in that collection
+                if let snapshotDocuments = querySnapshot?.documents {
+                    for doc in snapshotDocuments {
+                        let data = doc.data()
+                        //used a conditional downcast to cast (sender) into a string. Before the conditional downcast it was a optional Any?
+                        //When you use if let, you're essentially saying, "If the optional contains a value, unwrap it and assign it to a new constant (or variable), and only then execute the code inside the braces."
+                        //The data dictionary appears to have optional values, which means they might or might not contain a value. Using if let ensures that sender and messageBody are only assigned values if the respective dictionary lookups succeed and the values can be cast to String.
+                        //By using a comma , you're chaining multiple optional bindings together. This means that the code block inside the braces will only execute if both conditions are true: data[Constants.FStore.senderField] can be downcast to a String and assigned to sender, and data[Constants.FStore.bodyField] can also be downcast to a String and assigned to messageBody.
+                        if let messageSender = data[Constants.FStore.senderField] as? String,
+                           //Also conditional downcast to String
+                            let messageBody = data[Constants.FStore.bodyField] as? String {
+                            //newMessage is ready to add to the array of messages which is currently empty
+                            let newMessage = Message(sender: messageSender, body: messageBody)
+                            //tapping into the messages array and appending new messages to it
+                            self.messages.append(newMessage)
+                            DispatchQueue.main.async {
+                                //this is manipulating the user interface
+                                //what this does it taps into the tableView and triggers the data source methods again
+                                self.tableView.reloadData()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
     @IBAction func sendPressed(_ sender: UIButton) {
         //messageBody - stores the messaga body
         //if messageTextfield.text is not nil, save it inside messageBody
@@ -85,6 +117,4 @@ extension ChatViewController : UITableViewDataSource {
         //return the cell and it will be slotted into the tableview
         return cell
     }
-    
-    
 }
